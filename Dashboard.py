dae94c681cdbc4f727f897f8b16fc98b4121b764
firmanh3200 <135565052+firmanh3200@users.noreply.github.com>
2024-09-23 20:39:36 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,33 +6,8 @@ import openpyxl
 st.set_page_config(layout='wide')
 
 with st.container(border=True):
-    st.title(":green[Bahan Grafik Kecamatan Dalam Angka]")
+    st.title(":green[Kecamatan di Jawa Barat Dalam Visualisasi Data]")
 #st.subheader("", divider='rainbow')
-
-st.subheader(":blue[LATAR BELAKANG]")
-with st.expander("Latar Belakang"):
-    st.info("Publikasi Kecamatan Dalam Angka harus dapat terbit tepat waktu dan berkualitas. \
-        Keterbatasan sumber data, waktu dan tenaga penyusun publikasi sangat mempengaruhi \
-        kualitas dari publikasi.")
-
-st.subheader(":red[PERMASALAHAN]")
-with st.expander("Permasalahan"):
-    st.warning("Selama ini para penyusun publikasi Kecamatan Dalam Angka melakukan pengumpulan data \
-        secara parsial, tanpa memperhatikan konsistensi dan kewajaran data antar wilayah. \
-            Selain itu, para penyusun melakukan pembuatan grafik masing-masing, sehingga \
-                waktu dan tenaga menjadi kurang efektif/ efisien.")
-
-st.subheader(":green[SOLUSI ALTERNATIF]")
-with st.expander("Solusi Alternatif"):
-    st.success("Dashboard ini dibangun sebagai alternatif solusi sementara untuk mengefisienkan \
-        waktu, dan mengefektifkan tenaga para penyusun publikasi Kecamatan Dalam Angka, sehingga mereka dapat \
-            dengan mudah memilih jenis dan warna grafik dan mengunduhnya untuk dipasang \
-                di publikasi masing-masing. Selain itu, tersedia pula tabel dan keterangan sumber data diperoleh.")
-
-st.subheader(":green[TUJUAN]")
-with st.expander("Tujuan"):
-    st.success("Koordinasi dan Kerjasama antara BPS sebagai Pembina Data, Diskominfo sebagai Walidata, dan Dinas/ Badan/ Kantor/ \
-        Kecamatan/ Kelurahan sebagai Produsen Data dalam Mewujudkan Satu Data Indonesia.")
 
 st.subheader(":green[SUMBER DATA]")
 with st.expander("Sumber Data"):
